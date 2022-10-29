742d2616e970e92dc437cda0fbc93bec77ec9e66
unknown <shahirlatif1@gmail.com>
2022-10-29 00:08:58 -0500

    Loading icon, cooldown, download link

--- static/js/script.js
+++ static/js/script.js
@@ -1,14 +1,44 @@
 const goButton = document.getElementById("goButton");
+const audioWrapper = document.getElementById("audioWrapper");
+const loadingSpinner = document.getElementById("loadingspinner");
+const audioElement = document.getElementById("audioElement");
+const downloadButton = document.getElementById("downloadButton");
 
 goButton.onclick = () => {
+	buttonCooldown();
+
+	hide(audioElement);
+	show(loadingSpinner);
+	hide(downloadButton);
+
 	fetch("/generate")
 		.then(response => response.json())
 		.then(data => {
-			const audioElement = document.getElementById("audioElement");
-			const audioSource = document.getElementById("audioSource");
 			console.log(data.info);
+
+			loadingSpinner.style.display = "none";
+
+			const audioSource = document.getElementById("audioSource");
 			audioSource.src = "/audio/" + data.fileid;
 			audioElement.load();
 			audioElement.play();
+			audioElement.style.display = "block";
+			downloadButton.href = audioSource.src;
+			show(downloadButton);
 		});
+}
+
+function hide(element) {
+	element.style.display = "none";
+}
+
+function show(element) {
+	element.style.display = "block";
+}
+
+function buttonCooldown() {
+	goButton.disabled = true;
+	setTimeout(() => {
+		goButton.disabled = false;
+	}, 10000);
 }
