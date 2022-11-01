f7581c9343ac3bce489feae7c5312d673fcd2aaa
unknown <shahirlatif1@gmail.com>
2022-10-31 22:40:22 -0500

    Spectrum visualizer

--- static/js/script.js
+++ static/js/script.js
@@ -4,11 +4,41 @@ const loadingSpinner = document.getElementById("loadingspinner");
 const audioElement = document.getElementById("audioElement");
 const downloadButton = document.getElementById("downloadButton");
 
+const vis = document.getElementById("visualizer");
+const ctx = vis.getContext("2d");
+ctx.scale(1, -1);
+ctx.translate(0, -vis.height);
+ctx.fillStyle = "white";
+
+const ac = new (window.AudioContext || window.webkitAudioContext)();
+let aSrc = ac.createMediaElementSource(audioElement);
+let as = ac.createAnalyser();
+aSrc.connect(as);
+as.connect(ac.destination);
+as.fftSize = vis.width;
+const bufferLength = as.frequencyBinCount;
+const data = new Uint8Array(bufferLength);
+
+const lowLimit = 3;
+function animate() {
+	ctx.clearRect(0, 0, vis.width, vis.height);
+	as.getByteFrequencyData(data);
+	for (let x = 0; x < bufferLength; x++) {
+		let barHeight = data[x];
+		if (x < lowLimit) {
+			barHeight *= (x + 1) / lowLimit;
+		}
+		ctx.fillRect(2 * x, 0, 2, barHeight / 255 * (vis.height - 4));
+	}
+	requestAnimationFrame(animate);
+}
+
 goButton.onclick = () => {
 	buttonCooldown();
 
 	hide(audioElement);
 	show(loadingSpinner);
+	hide(vis);
 	hide(downloadButton);
 
 	fetch("/generate")
@@ -25,6 +55,8 @@ goButton.onclick = () => {
 			audioElement.style.display = "block";
 			downloadButton.href = audioSource.src;
 			show(downloadButton);
+			show(vis);
+			animate();
 		});
 }
 
